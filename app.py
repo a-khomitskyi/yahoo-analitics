b7fb124fdafa45a31a6eae4881d28ac38ba3a5a5
Andrii Khomitskyi <homitskiy.anriy@gmail.com>
2021-08-18 16:32:44 +0300

    v0.3

--- app.py
+++ app.py
@@ -1,27 +1,51 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response
 import psycopg2
-from main import files
+import main
 
 app = Flask(__name__)
 
-def get_content_from_DB(name):
+def get_content_from_table(name):
+	"""
+	:param name: Must be name of existing table in our DB
+	:return: All the data that collected from current DB table
+	"""
 	conn = psycopg2.connect('host=chunee.db.elephantsql.com dbname=qdljldfw user=qdljldfw password=TPKALru4l7bcPn2hpOz4nhKNp2hmxOQi')
 	cur = conn.cursor()
-
-	cur.execute(f"SELECT array_to_json(array_agg(row_to_json (r))) FROM (SELECT id, date, round(open::numeric, 6) as open, round(high::numeric, 6) as high, round(low::numeric, 6) as low, round(close::numeric, 6) as close, round(adj_close::numeric, 6) as adj_close, volume FROM {name}) r;")
+	# Correction of accuracy of floating point numbers
+	cur.execute(f"SELECT array_to_json(array_agg(row_to_json (r))) FROM (SELECT date, round(open::numeric, 6) as open, round(high::numeric, 6) as high, round(low::numeric, 6) as low, round(close::numeric, 6) as close, round(adj_close::numeric, 6) as adj_close, volume FROM {name}) r;")
+	# Get the result after executing sql query
 	res = cur.fetchone()[0]
 	# Close connection
 	conn.close()
 	return res
 
 
+# Forming json response with all the data from DB (list(dict<table_name: table_data>))
+@app.route('/api', methods=['GET'])
+def json_responce_all_data():
+	content = []
+	for i in main.files:
+		name = i
+		content.append({
+			name: get_content_from_table(i.lower())
+		})
+	return jsonify(content)
+
+
+# Forming json response with all the data from table (list(dict<column_name: column_data>))
 @app.route('/api/<string:comp>', methods=['GET'])
-def get_route(comp):
-	if comp.upper() in files:
-		return jsonify(get_content_from_DB(comp))
+def current_json_responce(comp):
+	# route exist
+	if comp.upper() in main.files:
+		return jsonify(get_content_from_table(comp))
+	# route not exist
 	else:
 		return make_response(jsonify({'error': 'Not correct route'}), 404)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	# main.rename_headers()
+	if main.insert_data_to_db() == 1:
+		app.run(debug=True)
+	else:
+		raise Exception('Something wrong...')
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import psycopg2
 from os import listdir
-import json
-import datetime
+import psycopg2
 
-files = [x.replace('.csv', '') for x in listdir('yahoo_data')]
+
+files = [x.replace('.csv', '') for x in listdir('yahoo_data')]  # get names of csv files
 
 
 def rename_headers():
+	"""
+	Need to rename column name in csv as one word
+	:return: *.csv with header 'Adj Close' that replaced on 'Adj_Close'
+	"""
 	for file in files:
 		r_file = open(f'yahoo_data/{file}.csv')
 		lines = r_file.readlines()
@@ -17,6 +20,10 @@ def rename_headers():
 
 
 def insert_data_to_db():
+	"""
+	Method to add data from existing *.csv files to the database tables
+	:return 1 if work finished correctly
+	"""
 	conn = psycopg2.connect(
 		'host=chunee.db.elephantsql.com dbname=qdljldfw user=qdljldfw password=TPKALru4l7bcPn2hpOz4nhKNp2hmxOQi')
 	cur = conn.cursor()
@@ -26,14 +33,7 @@ def insert_data_to_db():
 			next(f)  # Skip the header row.
 			# f , <database name>, Comma-Seperated
 			cur.copy_from(f, f'{file.lower()}', sep=',')
-			# Add new column as PK with name ID
-			cur.execute(f'ALTER TABLE {file.lower()} ADD COLUMN ID SERIAL PRIMARY KEY;')
 			conn.commit()
 	# Close connection
 	conn.close()
-
-
-if __name__ == '__main__':
-	# rename_headers()
-	insert_data_to_db()
-	pass
+	return 1
